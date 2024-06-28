82a371b01a9793ca6b5bd53f1158bebd816888c3
yelkendenenadam <mustafamertyelken@gmail.com>
2024-06-28 11:22:47 +0300

    initial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: "full",
+    redirectTo: 'order-summary'
+  },
+  {
+    path: 'order-summary',
+    loadComponent: () => import('./page/order-summary-page/order-summary-page.component') .then(m => m.OrderSummaryPageComponent),
+    title: 'Order Summary'
+  }
+];
