01119bc47c215391c681a3ce58093240cf7cf774
yelkendenenadam <mustafamertyelken@gmail.com>
2024-07-04 17:42:51 +0300

    initial commit

--- src/app/directive/hightlight.directive.spec.ts
+++ src/app/directive/hightlight.directive.spec.ts
@@ -2,6 +2,7 @@ import { HightlightDirective } from './hightlight.directive';
 
 describe('HightlightDirective', () => {
   it('should create an instance', () => {
+    // @ts-ignore
     const directive = new HightlightDirective();
     expect(directive).toBeTruthy();
   });
